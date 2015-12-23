42be2c1cb7535202b54fbc839594a18bc4928a55
melanselmo@gmail.com <melissaanselmo@melissas-air.home>
2015-12-23 09:54:23 -0500

    First commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,63 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    /*--- Universal variables ---*/
+
+var guessCount = 0;
+var correctGuess = false;
+var num = Math.floor((Math.random() * 100) + 1); 
+var finish = false;
+
+  /*--- Generate Random Number ---*/
+
+console.log(num);
+
+  /*--- User submits #userGuess using #guessButton ---*/
+$('form').submit(function() {
+  var userGuess =$('#userGuess').val();
+    console.log(num);
+    console.log(userGuess);
+    guessCount += 1;
+  $("#count").text(guessCount);
+  $('#guessList ul').append('<li>'+ userGuess + '</li>');
+    });
+  var guessList = "<li>" + userGuess + "</li>";
+  var difference = userGuess - num;
+  if (userGuess == num) {
+    $('#feedback').append("You win! You guessed the right Number!");
+    correctGuess = true;
+    } else if (difference >= 1 && difference <= 10) {
+    $('#feedback').text('VERY Hot');
+    } else if (difference < 10 && difference <= 20){
+    $('#feedback').text('Hot');
+    } else if (difference < 20 && difference <= 30){
+    $('#feedback').text('Warm');
+    } else if (difference < 30 && difference <= 50){
+    $('#feedback').text('Cool');
+    } else if ( difference > 50){
+    $('#feedback').text('Ice Cold');
+    };
+
+    return false;
+ 
 });
+
+
+/*---- To start a new game ----*/
+    $('.new').click(function(){
+        newGame();
+    });
+
+/*--- Function that resets game --*/
+    function newGame(){
+        guessCount = 0;
+        finish = false;
+        $('#userGuess').val('');
+        $('#count').text(guessCount);
+        $('#guessList li').remove();
+        num = (Math.floor(Math.random()*10));
+        console.log("it works! new secret number is " + num);
+    }
+
 
 
